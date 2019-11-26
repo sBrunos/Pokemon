5c2390ce5db3e470a9927261e36401f8cd5b99d1
Bruno <ba160497@gmail.com>
2019-11-26 18:20:12 -0200

    Método battle quase implementado

--- src/pucrs/ep/poo/cartas/modelo/PokemonCard.java
+++ src/pucrs/ep/poo/cartas/modelo/PokemonCard.java
@@ -1,11 +1,14 @@
 package pucrs.ep.poo.cartas.modelo;
 
+import java.util.ArrayList;
+
 public class PokemonCard extends Card{
     private int ataque;
     private int vida;
-    private enum elemento {FOGO, AGUA, TERRA, VOADOR, GRAMA, NORMAL, VENENO, ELETRICO, PEDRA, INSETO};
+    public enum elemento {FOGO, AGUA, TERRA, VOADOR, GRAMA, NORMAL, VENENO, ELETRICO, PEDRA, INSETO};
     private elemento e;
-
+    private ArrayList<elemento> vantagem;
+    private boolean morto = false;
 
     public PokemonCard(int n) {
         super(n);
@@ -24,31 +27,44 @@ public class PokemonCard extends Card{
                 case 1: super.setVida(10);
                     super.setAtaque(5);
                     e = elemento.AGUA;
+                    vantagem.add(elemento.FOGO);
                     break;
                 case 2: super.setVida(15);
                     super.setAtaque(2);
                     e = elemento.GRAMA;
+                    vantagem.add(elemento.PEDRA);
+                    vantagem.add(elemento.TERRA);
                     break;
                 case 3: super.setVida(8);
                     super.setAtaque(8);
                     e = elemento.INSETO;
+                    vantagem.add(elemento.GRAMA);
+
                     break;
                 case 4: super.setVida(15);
                     super.setAtaque(7);
                     e = elemento.FOGO;
+                    vantagem.add(elemento.GRAMA);
+                    vantagem.add(elemento.INSETO);
+                    vantagem.add(elemento.VENENO);
                     break;
                 case 5: super.setVida(11);
                     super.setAtaque(4);
                     e = elemento.PEDRA;
-
+                    vantagem.add(elemento.VOADOR);
+                    vantagem.add(elemento.ELETRICO);
                     break;
                 case 6: super.setVida(9);
                     super.setAtaque(12);
                     e = elemento.VOADOR;
+                    vantagem.add(elemento.INSETO);
+                    vantagem.add(elemento.GRAMA);
+                    vantagem.add(elemento.TERRA);
                     break;
                 case 7: super.setVida(8);
                     super.setAtaque(6);
                     e = elemento.ELETRICO;
+                    vantagem.add(elemento.VOADOR);
                     break;
                 case 8: super.setVida(15);
                     super.setAtaque(8);
@@ -57,12 +73,34 @@ public class PokemonCard extends Card{
                 case 9: super.setVida(14);
                     super.setAtaque(7);
                     e = elemento.TERRA;
+                    vantagem.add(elemento.ELETRICO);
                     break;
                 case 10: super.setVida(12);
                     super.setAtaque(6);
                     e = elemento.VENENO;
+                    vantagem.add(elemento.GRAMA);
                     break;
             }
         }
+    }
+
+    public ArrayList<elemento> getVantagem() {
+        return vantagem;
+    }
+
+    public void setVantagem(ArrayList vantagem) {
+        this.vantagem = vantagem;
+    }
+
+    public void addVantagem(elemento tipo){
+        vantagem.add(tipo);
+    }
+
+    public void addAtaque(){
+        ataque += 5;
+    }
+
+    public void setMorto(boolean morto) {
+        this.morto = morto;
     }
 }
